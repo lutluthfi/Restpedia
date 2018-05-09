c0093bdd5c01dcb61368712c66fa6b297cee3c62
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2018-05-10 00:14:56 +0700

    Clean Login and Register

--- app/src/main/java/com/brawijaya/filkom/restpedia/ui/RegisterActivity.java
+++ app/src/main/java/com/brawijaya/filkom/restpedia/ui/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.brawijaya.filkom.restpedia.ui;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,53 +18,41 @@ import com.brawijaya.filkom.restpedia.ui.base.BaseActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RegisterActivity extends BaseActivity {
-    private static final String TAG = "SignupActivity";
+public class RegisterActivity extends BaseActivity implements View.OnClickListener{
 
-    @BindView(R.id.input_name)
-    EditText _nameText;
-    @BindView(R.id.input_address) EditText _addressText;
-    @BindView(R.id.input_email) EditText _emailText;
-    @BindView(R.id.input_mobile) EditText _mobileText;
-    @BindView(R.id.input_password) EditText _passwordText;
-    @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
-    @BindView(R.id.btn_signup) Button _signupButton;
-    @BindView(R.id.link_login) TextView _loginLink;
+    @BindView(R.id.input_name) EditText mName;
+    @BindView(R.id.input_address) EditText mAddress;
+    @BindView(R.id.input_email) EditText mEmail;
+    @BindView(R.id.input_mobile) EditText mMobile;
+    @BindView(R.id.input_password) EditText mPassword;
+    @BindView(R.id.input_reEnterPassword) EditText mPasswordConfirmation;
+    @BindView(R.id.btn_register) Button mRegister;
+    @BindView(R.id.link_login) TextView mLogin;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        ButterKnife.bind(this);
-
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signup();
-            }
-        });
-
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-                finish();
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-            }
-        });
+        setUnbinder(ButterKnife.bind(this));
+        mRegister.setOnClickListener(this);
+        mLogin.setOnClickListener(this);
     }
 
     public void signup() {
-        Log.d(TAG, "Signup");
 
-        if (!validate()) {
+        String name = mName.getText().toString();
+        String address = mAddress.getText().toString();
+        String email = mEmail.getText().toString();
+        String mobile = mMobile.getText().toString();
+        String password = mPassword.getText().toString();
+        String passwordConfirmation = mPasswordConfirmation.getText().toString();
+
+        if (!validate(name, address, email, mobile, password, passwordConfirmation)) {
             onSignupFailed();
             return;
         }
 
-        _signupButton.setEnabled(false);
+        mRegister.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this,
                 R.style.AppTheme_Dark_Dialog);
@@ -71,29 +60,20 @@ public class RegisterActivity extends BaseActivity {
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
-        String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
-        String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
-
         // TODO: Implement your own signup logic here.
 
-        new android.os.Handler().postDelayed( new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onSignupSuccess();
+                progressDialog.dismiss();
+            }
+        }, 3000);
     }
 
 
     public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
+        mRegister.setEnabled(true);
         setResult(RESULT_OK, null);
         finish();
     }
@@ -101,60 +81,53 @@ public class RegisterActivity extends BaseActivity {
     public void onSignupFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-        _signupButton.setEnabled(true);
+        mRegister.setEnabled(true);
     }
 
-    public boolean validate() {
+    public boolean validate(String name, String address, String email, String mobile, String password, String passwordConfirmation) {
         boolean valid = true;
 
-        String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
-        String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
-
         if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
+            mName.setError("at least 3 characters");
             valid = false;
         } else {
-            _nameText.setError(null);
+            mName.setError(null);
         }
 
         if (address.isEmpty()) {
-            _addressText.setError("Enter Valid Address");
+            mAddress.setError("enter valid address");
             valid = false;
         } else {
-            _addressText.setError(null);
+            mAddress.setError(null);
         }
 
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            mEmail.setError("enter a valid email address");
             valid = false;
         } else {
-            _emailText.setError(null);
+            mEmail.setError(null);
         }
 
         if (mobile.isEmpty() || mobile.length()!=10) {
-            _mobileText.setError("Enter Valid Mobile Number");
+            mMobile.setError("enter valid mobile number");
             valid = false;
         } else {
-            _mobileText.setError(null);
+            mMobile.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            mPassword.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
-            _passwordText.setError(null);
+            mPassword.setError(null);
         }
 
-        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
-            _reEnterPasswordText.setError("Password Do not match");
+        if (!(passwordConfirmation.equals(password))) {
+            mPasswordConfirmation.setError("password doesn't match");
             valid = false;
         } else {
-            _reEnterPasswordText.setError(null);
+            mPasswordConfirmation.setError(null);
         }
 
         return valid;
@@ -163,5 +136,22 @@ public class RegisterActivity extends BaseActivity {
     @Override
     public void setupView() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_register:
+                signup();
+                break;
+            case R.id.link_login:
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+                break;
+            default:
+                break;
+        }
     }
 }
